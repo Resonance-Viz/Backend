17936c30c0ce4c141405e2c75640c0b07cfa8b09
GortFan <k.trichiliocosm@gmail.com>
2024-05-26 20:44:50 -0400

    .

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
@@ -7,13 +6,5 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-def get_ec2_public_ip():
-    return os.getenv('EC2_PUBLIC_DNS')
-
 if __name__ == '__main__':
-    ec2_ip = get_ec2_public_ip()
-    
-    if ec2_ip:
-        app.run(host='0.0.0.0', port=5000)  
-    else:
-        raise Exception("EC2 IP was not found in environment variables")
+    app.run(host='0.0.0.0', port=5000)  
