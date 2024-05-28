2ed58308060eb62486833722dd7f13e76b0313b8
GortFan <k.trichiliocosm@gmail.com>
2024-05-28 14:56:14 -0400

    .

--- app.py
+++ app.py
@@ -19,18 +19,19 @@ def hello_world():
 
 @app.route('/formatted-data')
 def get_data():
-    # Create a new client and connect to the server
-    client = MongoClient(mongo_uri, server_api=ServerApi('1'))
+    return 'hi'
+    # # Create a new client and connect to the server
+    # client = MongoClient(mongo_uri, server_api=ServerApi('1'))
     
-    try:
-        client.admin.command('ping')
-        print("Pinged your deployment. You successfully connected to MongoDB!")
-    except Exception as e:
-        print(e)
+    # try:
+    #     client.admin.command('ping')
+    #     print("Pinged your deployment. You successfully connected to MongoDB!")
+    # except Exception as e:
+    #     print(e)
     
-    collection = client['mock_ww_data']['character_usage']
+    # collection = client['mock_ww_data']['character_usage']
 
-    return dumps(collection.find())
+    # return dumps(collection.find())
     
 
 if __name__ == '__main__':
