a9ab78ef15e48d08897933507eb1a0f53fa5642b
GortFan <k.trichiliocosm@gmail.com>
2024-05-28 17:43:38 -0400

    forgot there was an extra db instance in method xd

--- app.py
+++ app.py
@@ -29,14 +29,6 @@ def create_app():
     @app.route('/formatted-data')
     def get_data():
         # Create a new client and connect to the server
-        client = MongoClient(mongo_uri, server_api=ServerApi('1'))
-        
-        try:
-            client.admin.command('ping')
-            print("Pinged your deployment. You successfully connected to MongoDB!")
-        except Exception as e:
-            print(e)
-        
         collection = client['mock_ww_data']['character_usage']
 
         return dumps(collection.find())
